e3b7a0a0f5275d583ee8071c34cba1eeb26e4cb8
Patrick Luboobi <patricklu2010@gmail.com>
2017-07-04 13:44:42 +0300

    Write tests for arg more, equal or < 100

--- test_data_types.py
+++ test_data_types.py
@@ -11,6 +11,15 @@ class Datatype(unittest.TestCase):
     def test_return_when_argument_is_bool(self):
         self.assertEqual(data_type(True), True)
 
+    def test_argument_less_than_100 (self):
+        self.assertEqual(data_type(5), 'less than 100')
+
+    def test_argument_more_than_100 (self):
+        self.assertEqual(data_type(115), 'more than 100')
+
+    def test_argument_equal_to_100 (self):
+        self.assertEqual(data_type(115), 'more than 100')
+
 
 if __name__ == '__main__':
     unittest.main()
