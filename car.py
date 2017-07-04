f158cb5eaebdd7749e98cfc91a27520356294b6f
Patrick Luboobi <patricklu2010@gmail.com>
2017-07-04 14:42:16 +0300

    Initialize car class constructor

--- car.py
+++ car.py
@@ -1,2 +1,21 @@
 class Car(object):
-    pass
+    """
+    Class to represent a car object
+    Attributes:
+        name: name of car
+        model: car model
+        type: car type
+    """
+
+    def __init__(self, name="General", model="GM", type="saloon"):
+        """
+        initialises Car with default values for name, model and type
+
+        :param name:
+        :param model:
+        :param type:
+        """
+        self.type = type
+        self.model = model
+        self.name = name
+        self.speed = 0
